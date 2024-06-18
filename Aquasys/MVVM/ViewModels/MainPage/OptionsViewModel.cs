c2b2a51309f62575f8d02e2c9097fe71a32742b2
Leonardo de Meira Oliveira <106771518+LeonardX7@users.noreply.github.com>
2024-06-18 01:56:21 -0300

    Ajustando o Flyout Menu

---
+++ Aquasys/MVVM/ViewModels/MainPage/OptionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Aquasys.Core.Entities;
+using Aquasys.Core.Utils;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Aquasys.MVVM.ViewModels.MainPage
+{
+    public partial class OptionsViewModel : BaseViewModels
+    {
+        [ObservableProperty]
+        public User user;
+
+        public OptionsViewModel() 
+        {
+            user = ContextUtils.ContextUser;
+        }
+    }
+}
